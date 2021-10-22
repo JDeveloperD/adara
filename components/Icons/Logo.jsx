fde48c29c6416c136f58dcb0913ce52c6eb88a52
David Sandoval <jsandovalbd1993@gmail.com>
2021-10-22 15:38:25 -0500

    comments were added on components

--- components/Icons/Logo.jsx
+++ components/Icons/Logo.jsx
@@ -1,3 +1,10 @@
+/**
+ * Logo
+ * 
+ * version 0.1
+ * 
+ */
+
 import React from 'react'
 
 const Logo = ({ color, props }) => {
--- layouts/DefaultLayout.jsx
+++ layouts/DefaultLayout.jsx
@@ -3,7 +3,6 @@
  * 
  * version 0.1
  * 
- * 
  */
 
 import React from 'react'
